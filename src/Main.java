890b98ce474ca5a6c726fc679e6e1580472dcd57
adelibeeva27 <adelibeeva27@acsbg.org>
2025-09-26 11:09:11 +0300

    Classwork String

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello, what is your name?");
+        String name = scanner.nextLine();
+        System.out.println(name.length());
+        System.out.println(name.substring(0,3));
+        System.out.println(name.equals("Java"));
+        System.out.println(name.compareTo("Java"));
+
     }
 }
